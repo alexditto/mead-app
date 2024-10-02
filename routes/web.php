10a91bd4c2e527b03ac270f47373d3aa7d78d4a4
alexditto <alexdittocareer@gmail.com>
2024-10-02 00:36:18 -0500

    blueprint

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/{any}', function () {
-    return redirect('/admin');
+
+Route::fallback(function (){
+    return redirect('/admin/login');
 });
